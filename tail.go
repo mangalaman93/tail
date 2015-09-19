a228daaff765e44edceb5b0180ca7d2e350ef253
Aman Mangal <mangalaman93@gmail.com>
2015-09-18 22:02:52 -0400

    use bufio instead of scanner

--- tail.go
+++ tail.go
@@ -3,25 +3,31 @@ package tail
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"os/exec"
 	"syscall"
 	"time"
 )
 
+const (
+	QUEUE_SIZE = 100
+)
+
 type Tail struct {
 	Filename   string
-	BufferSize int64
+	BufferSize int
 	Lines      chan string
 	cmd        *exec.Cmd
 	wait       chan bool
 }
 
-func (t *Tail) String() string {
+func (t *Tail) string() string {
 	return fmt.Sprintf("&Tail{Filename:%s, BufferSize:%d}", t.Filename, t.BufferSize)
 }
 
-func TailFile(filepath string, buffersize int64) (*Tail, error) {
+func TailFile(filepath string, buffersize int) (*Tail, error) {
 	// check whether the file exists
 	_, err := os.Stat(filepath)
 	if err != nil {
@@ -42,15 +48,25 @@ func TailFile(filepath string, buffersize int64) (*Tail, error) {
 	t := &Tail{
 		Filename:   filepath,
 		BufferSize: buffersize,
-		Lines:      make(chan string, buffersize),
+		Lines:      make(chan string, QUEUE_SIZE),
 		cmd:        cmd,
 		wait:       make(chan bool, 1),
 	}
 
 	go func() {
-		scanner := bufio.NewScanner(reader)
-		for scanner.Scan() {
-			t.Lines <- scanner.Text()
+		bigreader := bufio.NewReaderSize(reader, buffersize)
+		line, isPrefix, err := bigreader.ReadLine()
+		for err == nil && !isPrefix {
+			t.Lines <- string(line)
+			line, isPrefix, err = bigreader.ReadLine()
+		}
+
+		if isPrefix {
+			log.Println("buffer size is too small!")
+		}
+
+		if err != io.EOF {
+			log.Println(err)
 		}
 
 		close(t.Lines)
