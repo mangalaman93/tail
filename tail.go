0e0868b9c534e96d8773740bb9de04345e44611d
Aman Mangal <mangalaman93@gmail.com>
2015-09-18 22:12:38 -0400

    add comments

--- tail.go
+++ tail.go
@@ -12,20 +12,22 @@ import (
 )
 
 const (
-	QUEUE_SIZE = 100
+	QUEUE_SIZE = 100 // size of channel
 )
 
 type Tail struct {
-	Filename string
-	Lines    chan string
-	cmd      *exec.Cmd
-	wait     chan bool
+	Filename string      // name of file to tail
+	Lines    chan string // channel to read lines
+	cmd      *exec.Cmd   // command object
+	wait     chan bool   // channel signal to stop waiting
 }
 
 func (t *Tail) String() string {
 	return fmt.Sprintf("&Tail{Filename:%s}", t.Filename)
 }
 
+// begins tailing a linux file. Output stream is
+// made available through `Tail.Lines` channel
 func TailFile(filepath string, buffersize int) (*Tail, error) {
 	// check whether the file exists
 	_, err := os.Stat(filepath)
@@ -74,6 +76,7 @@ func TailFile(filepath string, buffersize int) (*Tail, error) {
 	return t, nil
 }
 
+// stops tailing the file
 func (t *Tail) Stop() {
 	t.cmd.Process.Signal(syscall.SIGINT)
 	timeout := time.After(2 * time.Second)
